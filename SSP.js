3cd826cbd4e7de9f691e7b7e79e88855327b2256
nathsten <nathsten03@gmail.com>
2020-11-11 12:19:48 +0100

    Lagt til random Stein,saks,papir

--- SSP.js
+++ SSP.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const rndSSP = () => {
+    let SSP = ['stein', 'saks', 'papir'];
+    const rndTall = () => Math.floor(Math.random() * SSP.length);
+    let output = SSP[rndTall()];
+    return output;
+}
+
+console.log(rndSSP());
