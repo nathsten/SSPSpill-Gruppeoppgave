4c3e81bcdc6b71c6e845d6fbec1d10a893962553
nathsten <nathsten03@gmail.com>
2020-11-12 12:05:59 +0100

    Laget kobling til SSPDiv

--- SSP.js
+++ SSP.js
@@ -1,3 +1,12 @@
+//@ts-check
+
+//Forkortelse for document.getElementById("idName");
+const $ = (id) => document.getElementById(id);
+
+/**
+ * Funksjon som returnerer tilfeldig 'stein', 'saks', 'papir'.
+ * @returns {String}
+ */
 const rndSSP = () => {
     const SSP = ['stein', 'saks', 'papir'];
     const rndTall = () => Math.floor(Math.random() * SSP.length);
@@ -5,4 +14,20 @@ const rndSSP = () => {
     return output;
 }
 
-console.log(rndSSP());
+//Kobling til SSPDiv
+const SSPDiv = $("SSPdiv");
+
+SSPDiv.addEventListener("click", sjekkSSP);
+
+/**
+ * Hovedfunksjonen som finner ut hvile av SSP du har trykket på
+ * og kjører funksjonen som returnerer maskinen sitt svar
+ * @param {{ target: any; }} e
+ */
+function sjekkSSP(e){
+    // t blir den av de tre inputene du har trykket på
+    const t = e.target;
+    
+    // maskinSSP er maskinen sin stein / saks / papir.
+    const maskinSSP = rndSSP();
+}
